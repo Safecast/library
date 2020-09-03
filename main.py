328ce2be857de505fda8102a23d15cd8991e91e4
Jolene <jolene.lim14@gmail.com>
2020-09-03 20:18:34 +0800

    update with time chart

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 from flask import Flask, render_template, request, redirect, url_for
-import requests
+from elasticsearch import Elasticsearch
 import json
+import pandas as pd
+import configparser
+from datetime import date
+from wrangle import wrangle
 
 app = Flask(__name__)
+wrangle = wrangle()
 
 def get_data():
-    data = requests.get('https://api.safecast.org/measurements.json?distance=100&latitude=34.0522&longitude=-118.2437')
-    if data.status_code == 200:
-        data = json.loads(data.content)
-    else:
-        data = []
-    return ['raw', data]
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    es = Elasticsearch('https://{}:{}@40ad140d461d810ac41ed710b5c7a5b6.us-west-2.aws.found.io:9243/'.format(config['ELASTICSEARCH']['Username'], config['ELASTICSEARCH']['Password']))
+
+    # query
+    today = date.today()
+    today = today.strftime('%Y-%m-%d')
+    raw = es.search(index='', body= {'from': 0, 'size': 10000, 'query': {'bool': {'must': [{'range': {'when_captured': {'gte': '2020-08-31', 'format': 'yyyy-MM-dd'}}}, {'range': {'loc_lat': {'gte': 33, 'lte': 34.5}}}, {'range': {'loc_lon': {'gte': -118.5, 'lte': -117}}}, {'match': {'device_class': 'safecast'}}, {'exists': {'field': 'pms_pm02_5'}}]}}}, _source=['device', 'when_captured', 'loc_lat', 'loc_lon', 'pms_pm01_0', 'pms_pm02_5', 'pms_pm10_0'])
+    # full
+    full = [ms['_source'] for ms in raw['hits']['hits']]
+    full_sort = wrangle.sort_time(full)
+    #time = wrangle.wrangle_line(full)
+    # get latest
+    latest = wrangle.get_latest(full)
+
+    return ['raw', latest, full_sort]
 
 @app.route("/index")
 def index():
     data = get_data()
+
     return render_template('index.html', data=data)
 
 @app.route("/data", methods=['GET', 'POST'])
-def data():
+def filter(): 
+    data = get_data()
+    
     if request.method == 'POST':
-        bound = request.get_json()
-        print(bound)
-        bound = ['filtered', [{'latitude': 34.063467866218254, 'longitude': -118.23561034362642}], bound[0]]
+        bound = request.get_json()[0]
+        filtered = wrangle.is_within_custom(bound, data[2])
+        filtered_latest = wrangle.get_latest(filtered)
+        filtered_time = wrangle.sort_time(filtered)
+        bound = ['filtered', filtered_latest, filtered_time, bound]
         return render_template('map.html', data=bound) 
 
-    data = get_data()
+    return render_template('map.html', data=data)
+
+@app.route("/agg", methods=['POST'])
+def agg():
+    type = request.form.get("aggDropdown")
+    """
+    if type == 'avg':
+        pass
+    else if type == 'max':
+        pass
+    """
     return render_template('map.html', data=data)
 
 if __name__ == "__main__":
---
+++ wrangle.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Polygon, Point
+import json
+
+class wrangle:
+
+    def sort_time(self, data, time='when_captured'):
+        data = pd.DataFrame(data)
+        data[time] = pd.to_datetime(data[time], format='%Y-%m-%dT%H:%M:%SZ')
+        data = data.sort_values(time)
+        data[time] = data[time].dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+        data = data.to_json(orient='records')
+        return json.loads(data)
+
+    def wrangle_line(self, data, device='device'):
+        data = pd.DataFrame(data)
+        data = data.groupby(device).apply(lambda x: x.to_json(orient='records')).reset_index().rename(columns={0: 'values'})
+        data = json.loads(data.to_json(orient='records'))
+        return data
+
+    def get_latest(self, data, time='when_captured', device='device'):
+        data = pd.DataFrame(data)
+        data[time] = pd.to_datetime(data[time], format='%Y-%m-%dT%H:%M:%SZ')
+        data = data.sort_values(time, ascending=False).drop_duplicates(device)
+        data[time] = data[time].dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+        data = json.loads(data.to_json(orient='records'))
+        return data
+
+    def is_within_custom(self, bound, data):
+        bound = [(coord['lng'], coord['lat']) for coord in bound]
+        bound = Polygon(bound)
+
+        datasf = pd.DataFrame(data)
+        datasf['within'] = [True if Point(point['loc_lon'], point['loc_lat']).within(bound) else False for point in data]
+        filtered = datasf[datasf['within'] == True].to_json(orient='records')
+        filtered = json.loads(filtered)
+
+        return filtered
